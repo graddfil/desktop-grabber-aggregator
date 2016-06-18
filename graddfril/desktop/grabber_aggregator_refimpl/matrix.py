82739f6a2d728ed2b828fdd4ed8fa0294b0ee0f0
Andrey Tvorozhkov <atvorozhkov@yandex.ru>
2016-06-19 01:19:43 +0300

    Update matrix.py

--- graddfril/desktop/grabber_aggregator_refimpl/matrix.py
+++ graddfril/desktop/grabber_aggregator_refimpl/matrix.py
@@ -21,6 +21,7 @@ class MatrixClient:
         self.api = MatrixHttpApi(self.server, token=self.token)
         self.api.initial_sync()
 
+
     def send_event(self, event_type: str, content: dict):
         """Send an event of arbitrary type."""
         return self.api.send_message_event(self.room, event_type, content)
